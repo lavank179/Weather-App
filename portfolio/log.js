a5c2a40a13e51bb0f39c79e72ac5d24b54db72a4
lavank179 <chlavankumar179@gmail.com>
2021-01-11 23:24:26 +0530

    removed direct hyperlinks

--- portfolio/log.js
+++ portfolio/log.js
@@ -1,11 +1,30 @@
-  $('a[href*="#"]').on('click', function (e) {
-    e.preventDefault()
-  
-    $('html, body').animate(
-      {
-        scrollTop: $($(this).attr('href')).offset().top,
-      },
-      800,
-      'linear'
-    )
+  $('#home a[href*="#"]').on('click', function(e) {
+      e.preventDefault()
+
+      $('html, body').animate({
+              scrollTop: $($(this).attr('href')).offset().top,
+          },
+          800,
+          'linear'
+      )
   });
+
+
+
+  function post_form() {
+      var name = $("#contactName").val();
+      var mail = $("#contactEmail").val();
+      var messages = $("#contactMessage").val();
+      $.ajax({
+              method: "POST",
+              url: "http://lavankumarch.heliohost.org/portContact.php",
+              data: { name, mail, messages }
+          })
+          .done(function(msg) {
+              alert("Data Saved: " + msg);
+          });
+  }
+
+  function download_resume() {
+      window.location.href = "https://drive.google.com/uc?export=download&id=1-WEmFIYs7h3XELKCmPLkHVRnE9gGJVya";
+  }

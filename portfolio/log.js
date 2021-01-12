8e6ecc4f6851b1c8209946ce0083c2921a57204e
lavank179 <chlavankumar179@gmail.com>
2021-01-12 16:31:07 +0530

    added ajax for contact form

--- portfolio/log.js
+++ portfolio/log.js
@@ -9,26 +9,6 @@
       )
   });
 
-
-
-  function post_form() {
-      var name = $("#contactName").val();
-      var mail = $("#contactEmail").val();
-      var messages = $("#contactMessage").val();
-
-      $.ajax({
-          url: "http://lavankumarch.heliohost.org/portContact.php",
-          type: "POST",
-          async: true,
-          data: { contactName: name, contactEmail: mail, contactMessage: messages },
-          success: function(response) {
-              var res = JSON.parse(response);
-              console.log(res[0].message);
-              alert(res);
-          }
-      });
-  }
-
   function download_resume() {
       window.location.href = "https://drive.google.com/uc?export=download&id=1-WEmFIYs7h3XELKCmPLkHVRnE9gGJVya";
   }
